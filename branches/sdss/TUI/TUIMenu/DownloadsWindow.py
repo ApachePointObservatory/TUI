e02875490fef8970dbf5a3e52d05756ac456a5f8
rowen <rowen@d99c647b-b30b-4308-b0a5-1e8d5c0a5d43>
2009-03-23 21:51:20 +0000

    Start fresh

---
+++ branches/sdss/TUI/TUIMenu/DownloadsWindow.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Downloads window
+
+2005-07-08 ROwen
+"""
+import RO.Alg
+import RO.Wdg.HTTPGetWdg
+
+_MaxLines = 100
+_MaxTransfers = 5
+
+def addWindow(tlSet, visible=False):
+    tlSet.createToplevel (
+        name = "TUI.Downloads",
+        defGeom = "+835+290",
+        wdgFunc = RO.Alg.GenericCallback(
+            RO.Wdg.HTTPGetWdg.HTTPGetWdg,
+            maxTransfers = _MaxTransfers,
+            maxLines = _MaxLines,
+            helpURL = "TUIMenu/DownloadsWin.html",
+        ),
+        visible = visible,
+    )
