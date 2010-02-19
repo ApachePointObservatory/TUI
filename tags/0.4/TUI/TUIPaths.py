fc509d16d6b8bd6d980f1f4e595732e32c61dbe7
rowen <rowen@d99c647b-b30b-4308-b0a5-1e8d5c0a5d43>
2010-02-19 18:46:20 +0000

    Tag version 0.4

---
+++ tags/0.4/TUI/TUIPaths.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""Get paths to various TUI-related directories.
+
+2004-07-09 ROwen
+2005-07-11 ROwen    Modified for changed RO.OS.getAppSuppDirs.
+2005-09-22 ROwen    Changed getAddPaths to getTUIPaths.
+                    Added getResourceDir.
+2007-01-19 ROwen    Made getAddPaths compatible with pyinstaller.
+2009-11-09 ROwen    Changed TUIAdditions to <ApplicationName>Additions
+"""
+import os
+import sys
+import RO.OS
+import TUI  # for TUI.__file__
+import TUI.Version
+
+AppAdditions = "%sAdditions" % (TUI.Version.ApplicationName,)
+
+def getAddPaths():
+    """Return a list of 0 or more paths to existing additions directories, in order: local, shared.
+
+    Additions are kept in two optional folders named <applicationName>Additions,
+    one in the standard application support location for shared files,
+    the other in the standard application support location for user files.
+
+    See RO.OS.getAppSuppDirs for details on where these are.
+    For unix, there is no standard application support location for shared files
+    so TUI's root directory is used, instead. This is the directory containing
+    the TUI package, e.g. tuiRoot contains TUI, RO and possibly <applicationName>Additions.
+    """
+    # start with the application support directories
+    # include TUI's root if unix (since it has no standard shared location)
+    appSuppDirs = RO.OS.getAppSuppDirs()
+    addPathList = [dir for dir in appSuppDirs if dir != None]
+    if RO.OS.PlatformName == "unix":
+        tuiRoot = os.path.dirname(RO.OS.getResourceDir(TUI))
+        addPathList.append(tuiRoot)
+
+    # look in subdir "<applicationName>Additions" of each of these dirs
+    addPathList = [os.path.join(path, AppAdditions) for path in addPathList]
+    
+    # remove nonexistent dirs
+    addPathList = [path for path in addPathList if os.path.isdir(path)]
+    
+    # remove duplicates
+    addPathList = RO.OS.removeDupPaths(addPathList)
+
+    return addPathList
+
+def getResourceDir(*args):
+    """Return the resource directory for a specified resource.
+    Input:
+    - one or more path elements (strings)
+    
+    For example:
+    getResourceDir("Sounds") returns the directory of TUI sounds
+    """
+    return RO.OS.getResourceDir(TUI, *args)
+
+
+if __name__ == "__main__":
+    print "TUI Additions =", getAddPaths()
+    print "TUI Sounds =", getResourceDir("Sounds")
---
+++ tags/0.4/TUI/Version.py
@@ -0,0 +1,5 @@
+VersionName = "1.7.0"
+VersionDate = "2010-01-11"
+ApplicationName = "TUI"
+
+VersionStr = " ".join((VersionName, VersionDate))
