113cfbeeb7d7e7fb66bccef27443fdd27f1b8e6b
rowen <rowen@d99c647b-b30b-4308-b0a5-1e8d5c0a5d43>
2010-03-08 22:56:06 +0000

    TUI 1.7.1b2
    Updated RO to fix a problem with the status bar showing command replies in the wrong color.

--- TUI/Version.py
+++ TUI/Version.py
@@ -1,5 +1,5 @@
-VersionName = "1.7.1b1"
-VersionDate = "2010-03-05"
+VersionName = "1.7.1b2"
+VersionDate = "2010-03-08"
 ApplicationName = "TUI"
 
 VersionStr = " ".join((VersionName, VersionDate))
