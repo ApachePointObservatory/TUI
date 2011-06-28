69e200d2f01b035a741aa783952cb608cb050cc6
rowen <rowen@d99c647b-b30b-4308-b0a5-1e8d5c0a5d43>
2011-06-28 00:14:07 +0000

    TUI/Help/Scripts/BuiltInScripts/NICFPSDitherPointSource.html - Standardizes the line endings (to avoid sftp file synch always uploading a new copy of the file;   thanks to Jason Downing of Yummy FTP support for explaining what was going on).
    TUI/Guide/GuideWdg.py
    - Changed Hold/Current and history behavior to reduce confusion between user-chosen
      FITS files and images received from the guider:
      - Current always shows an image from the guider, if one is available, else nothing.
        Formerly if you used Choose... to view a FITS file and you had never received any images
        from the guider, then the image you were viewing would stay up when you pressed Current.
      - The image history is reserved for images received from the guider. Images viewed using
        Choose... are not added to the image history. 
      - Added initial default values for Thresh and RadMult because when the guide actors
        are first started they don't report values for these parameters.

--- TUI/Version.py
+++ TUI/Version.py
@@ -1,5 +1,5 @@
-VersionName = "1.9.0b2"
-VersionDate = "2011-06-17"
+VersionName = "1.9.0b2+"
+VersionDate = "2011-06-26"
 ApplicationName = "TUI"
 
 VersionStr = " ".join((VersionName, VersionDate))
